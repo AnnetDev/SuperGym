c26fdfee156a2f4527a04d55ac235343a0db8568
Anna <annabaidikova92@gmail.com>
2024-11-07 21:24:49 +0100

    added styles for about, video.js

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,9 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+
+import { loadVideo } from './video.js';
+
+// Вызов функции для инициализации события
+loadVideo();
---
+++ source/js/video.js
@@ -0,0 +1,17 @@
+export function loadVideo() {
+  document.querySelector('.video__button').addEventListener('click', function() {
+    const videoContainer = document.querySelector('.video__container');
+
+    // Создаем iframe с ленивой загрузкой видео
+    const iframe = document.createElement('iframe');
+    iframe.src = 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1';
+    iframe.width = '100%';
+    iframe.height = '100%';
+    iframe.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
+    iframe.allowFullscreen = true;
+
+    // Очищаем placeholder и вставляем iframe
+    videoContainer.innerHTML = '';
+    videoContainer.appendChild(iframe);
+  });
+}
